6d5a3e57075b4b5f7636c00762c65948b37e4a9b
kulvir <kulvirdhillon240@gmail.com>
2020-01-09 21:33:15 -0500

    right swipe

--- exercise1/ViewController.swift
+++ exercise1/ViewController.swift
@@ -11,21 +11,72 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var box: UIImageView!
+    let heightOfscreen = UIScreen.main.bounds.size.height
+    let widthOfScreen = UIScreen.main.bounds.size.width
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     
         box.frame.origin.x = 0
         box.frame.origin.y  = 0
-        
-        
-        let heightOfscreen = UIScreen.main.bounds.size.height
         let time = heightOfscreen / 150
+        
+      
         UIView.animate(withDuration: TimeInterval(time)) {
-        self.box.frame.origin.y = heightOfscreen - self.box.frame.height
+            self.box.frame.origin.y = self.heightOfscreen - self.box.frame.height
         }
         
+        //swipe function
+      
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+              swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+              view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+               swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+               view.addGestureRecognizer(swipeLeft)
     }
+    
+    @objc func swiped(gesture: UISwipeGestureRecognizer) {
+           let swipeGesture = gesture as UISwipeGestureRecognizer
+       
+             
+//            self.box.frame.origin.x = self.widthOfScreen - self.box.frame.width
+        switch swipeGesture.direction {
+        case UISwipeGestureRecognizer.Direction.right:
+            UIView.animate(withDuration: 2, animations: {
+                 self.box.frame.origin.x = self.widthOfScreen - self.box.frame.width
+            }) { (true) in
+                UIView.animate(withDuration: 2, animations: {
+                     self.box.frame.origin.y = 0
+                }) { (true) in
+//                    UIView.animate(withDuration: 2) {
+//                        self.box.frame.origin.x = 0
+//                    }
+                    UIView.animate(withDuration: 2, animations: {
+                        self.box.frame.origin.x = 0
+                    }) { (true) in
+                        UIView.animate(withDuration: 2, animations:{
+                        self.box.frame.origin.y = self.heightOfscreen - self.box.frame.height
+                        }
+                    )}
+                }
+            }
+       case UISwipeGestureRecognizer.Direction.left:
+        UIView.animate(withDuration: 2) {
+            self.box.frame.origin.y = 0
+            }
+        default:
+            break
+        }
+           
+    }
+       
+        
+           
+       }
 
-}
+
 
